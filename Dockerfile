2c72d56109ca85f2a5721e5054f2c933249a82b8
Max Vasin <m.vasin@forpeople.company>
2024-05-23 08:03:44 +0300

    Increase RAM disk size

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM mongo:5
+FROM mongo:6
 
-ENV TMPFS_SIZE 1024
+ENV TMPFS_SIZE 2G
 ADD entrypoint.sh /entrypoint.sh
 ADD mongod.conf /etc/mongod.conf
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "Mounting tmpfs volume of size: ${TMPFS_SIZE}m"
-mount -t tmpfs -o size=${TMPFS_SIZE}m tmpfs /data/db
+mount -t tmpfs -o size=${TMPFS_SIZE} tmpfs /data/db
 
 exec "$@"
