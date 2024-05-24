9825b7d3ce5df78c7ae568fc8b70137f6530f755
Max Vasin <m.vasin@forpeople.company>
2024-05-24 14:22:32 +0300

    Fix descriptor limits

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,11 @@ set -e
 echo "Mounting tmpfs volume of size: ${TMPFS_SIZE}m"
 mount -t tmpfs -o size=${TMPFS_SIZE} tmpfs /data/db
 
+ulimit -f unlimited
+ulimit -t unlimited
+ulimit -v unlimited
+ulimit -n 524288
+ulimit -m unlimited
+ulimit -u 524288
+
 exec "$@"
